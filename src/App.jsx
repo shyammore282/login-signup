47b4d3ed6d66b3756d996d1ff539d988d1f04293
shyam more <skumarmoure@gmail.com>
2024-08-24 19:00:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Form from "./components/form/Form";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="container">
+      <Form />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/login/Login.jsx
@@ -0,0 +1,18 @@
+const Login = () => {
+  return (
+    <div>
+      <form action="">
+        <div className="input">
+          <label htmlFor="name">User Name</label>
+          <input type="text" name="" value="" placeholder="User Name" />
+        </div>
+        <div className="input">
+          <label htmlFor="name">Password</label>
+          <input type="text" name="" value="" placeholder="Password" />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
